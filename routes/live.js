ffae010031e7dffd60ee1e58355dade5890948d1
Dimitar Petkov <dimitar.petkov.ddp@gmail.com>
2020-12-30 15:44:04 +0200

    renamed index page to live page; other renamings

--- routes/live.js
+++ routes/live.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'TRENERGO' });
+  res.render('live', { title: 'TRENERGO' });
 });
 
 module.exports = router;
