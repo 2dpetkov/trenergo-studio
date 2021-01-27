1354f5103c72d6ee35d57c68e613be5ee46adccd
Dimitar Petkov <dimitar.petkov.ddp@gmail.com>
2021-01-27 23:25:08 +0200

    rearranged the browser tests

--- browser_tests/countDownTimerTests.js
+++ browser_tests/countDownTimerTests.js
@@ -1,3 +1,12 @@
 var assert = chai.assert;
 var expect = chai.expect;
 
+describe('countDownTimer', function () {
+    describe('#start', function () {
+        it('should end within timeot (TODO)', function (done) {
+            // TODO: define timer tests
+            done(); 
+            //var cdt = new CountDownTimer(10);
+        });
+    });
+});
